8c256fefd626daeae56c4d8da289968e05509a5e
EdwardKuok1 <ekuo0003@student.monash.edu>
2024-05-25 11:15:49 +1000

    A3 coomit 1

--- mode1.py
+++ mode1.py
@@ -1,5 +1,5 @@
 from landsites import Land
-
+from data_structures.bst import BinarySearchTree
 
 class Mode1Navigator:
     """
@@ -11,22 +11,53 @@ class Mode1Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        lands = BinarySearchTree()
+        for site in sites:
+            lands.__setitem__(site.guardians/site.gold, site)
+        self.sites = lands
+
+        self.adventurers = adventurers
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
-
+        return_list = []
+        death_count = 0
+        for site in self.sites:
+            if self.adventurers >= site.item.guardians:
+                return_list.append((site.item, site.item.guardians))
+                death_count += site.item.guardians
+            else:
+                return_list.append((site.item, self.adventurers))
+                death_count += self.adventurers
+                self.adventurers = death_count
+                break
+            self.adventurers -= site.item.guardians
+        return return_list
+            
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        result_list = []
+        for adventurer_num in adventure_numbers:
+            gold_result = 0
+            for site in self.sites:
+                if adventurer_num >= site.item.guardians:
+                    gold_result += site.item.gold
+                else:
+                    gold_result += site.item.gold * adventurer_num / site.item.guardians
+                    break
+                adventurer_num -= site.item.guardians
+            result_list.append(gold_result)
+        return result_list
 
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.sites.__delitem__(land.guardians/land.gold)
+        land.gold = new_reward
+        land.guardians = new_guardians
+        self.sites.__setitem__(land.guardians/land.gold, land)
--- mode2.py
+++ mode2.py
@@ -1,5 +1,5 @@
 from landsites import Land
-
+from data_structures.heap import MaxHeap
 
 class Mode2Navigator:
     """
@@ -11,16 +11,53 @@ class Mode2Navigator:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        self.team_count = n_teams
+        self.sites = []
+        self.scores = None
 
     def add_sites(self, sites: list[Land]) -> None:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        for site in sites:
+            self.sites.append(site)
+        self.scores = MaxHeap(len(self.sites))
 
     def simulate_day(self, adventurer_size: int) -> list[tuple[Land | None, int]]:
         """
         Student-TODO: Best/Worst Case
         """
-        raise NotImplementedError()
+        result = []
+        self.construct_score_data_structure(adventurer_size)
+        #for i in range(len(self.scores)):
+        #    print(self.scores.get_max())
+        for teams in range(self.team_count):
+            team_score, site, adventurers_remaining = self.scores.get_max()
+            print(team_score, site, adventurers_remaining)
+            if adventurers_remaining == adventurer_size:
+                result.append((None, adventurer_size))
+            else:
+                result.append((site, adventurer_size - adventurers_remaining))
+            print(result[teams])
+        return result
+
+    def comupte_score(self, site: Land, adventurer_size: int):
+        default_reward = adventurer_size*2.5
+        score = 0
+        if site.get_guardians() > 0 and site.get_gold() > 0:
+            adventurers_sent = min(adventurer_size, site.get_guardians())
+            reward = min(adventurers_sent*site.get_gold()/site.get_guardians(), site.get_gold())
+            adventurers_remaining = adventurer_size - adventurers_sent
+            score = 2.5*adventurers_remaining + reward
+
+        if default_reward > score:
+            return (default_reward, adventurer_size)
+        else:
+            site.gold -= reward
+            site.guardians -= adventurers_sent
+            return (score, adventurers_remaining)
+
+    def construct_score_data_structure(self, adventurer_size):
+        for site in self.sites:
+            score, adventurers_remaining = self.comupte_score(site, adventurer_size)
+            self.scores.add((score, site, adventurers_remaining))
