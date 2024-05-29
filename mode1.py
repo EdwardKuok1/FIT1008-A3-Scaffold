2554e46a2e35f556672ade22e1f22bcfcb7c4be1
EdwardKuok1 <ekuo0003@student.monash.edu>
2024-05-30 08:38:47 +1000

    commit

--- mode1.py
+++ mode1.py
@@ -3,13 +3,35 @@ from data_structures.bst import BinarySearchTree
 
 class Mode1Navigator:
     """
-    Student-TODO: short paragraph as per
-    https://edstem.org/au/courses/14293/lessons/46720/slides/318306
+    This class manages a list of 'land' and a specified number of adventurers and returns the max amount of gold that can be made, simulating different adventure scenarios and updating site details.
+    The data structures used consists of BinarySearchTree and some list and tuples for support to store temporary data.
+    The BST sorts and stores 'land' objects based on their guardians to gold ratio, which ensures the efficient retrieval and updating of sites.
+    Example: 
+        If the number of guardians stationed on the sites goes by the order [300, 100, 150, 50] and the adventurers given are 500. Then the adventurers will be split into 300, 100, 100 to raid the sites.
+    Complexity:
+        Initialization:
+            Best Case: O(log(N)) where N is the number of sites, if adding one site to an already balanced BST.
+            Worst Case: O(Nlog(N)) for N sites, where each insertion into the BST takes O(log(N)) and this is repeated N times.
+        select_sites:
+            Best Case: O(log(N)) if only one route is chosen in the BST
+            Worst Case: O(N) if the number of guardians in all sites is less than the number of adventurers, requiring traversal of all N nodes.
+        select_sites_from_adventurer_numbers:
+            Best Case: O(logN), when theres only 1 adventure number and 1 list to calculate, the bst only chooses 1 route which has a complexity of log(N) when theres N sites.
+            Worst Case: O(N*M) where N is the number of sites and M is the number of adventure numbers, with the outer loop running M times and the inner loop running N times
+        update_site:
+            Best and Worst Case: O(logN) complexity when there's n site while the bst itself is balance and only 1 route is chosen.
+
     """
 
     def __init__(self, sites: list[Land], adventurers: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+            Initiallizing a new Bst object and sort the lands according to their guardians to gold ratio.
+            Parameters:
+                sites: the sites to be stored and sorted in the bst
+                adventurers: the number of adventurers to raid all these sites.
+            Complexity:
+                best case: O(log(N)) where there's only 1 site to be added and theres N sites present in the bst
+                worst case: O(Nlog(N)) where there's N sites to be added and every sites needs log(N) steps to be sorted. It constructs the bst from the ground up.
         """
         lands = BinarySearchTree()
         for site in sites:
@@ -20,7 +42,12 @@ class Mode1Navigator:
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
-        Student-TODO: Best/Worst Case
+            This method selects the most efficient lands to raid with the initial adventurer number given.
+            Returns: a result list which contains tuples of the site and the amount of adventurers sent to this site.
+                The result list depends on how many initial adventurers are given to determine how many sites are raided
+            Complexity:
+                best case: O(log(N)): The bst chooses one route which has a depth of log(N) when theres N nodes in the bst (since the tree is already balanced)
+                worst case: O(N): All the guardians add up are still lesser than the amount of raiders, bst goes through all the nodes (N nodes)
         """
         return_list = []
         death_count = 0
@@ -38,7 +65,14 @@ class Mode1Navigator:
             
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
-        Student-TODO: Best/Worst Case
+            calculates the rewards with different adventurer numbers for every site
+            Parameters:
+                adventure_numbers: a list of adventure numbers to simulate the amount of gold stolen of every site for every adventurer number.
+            Returns:
+                result_list: a list which contains the potential amount of gold stolen from every site for every adventurer numbers.
+            Complexity:
+                best case: O(logN), when theres only 1 adventure number and 1 list to calculate, the bst only chooses 1 route which has a complexity of log(N) when theres N sites.
+                worst case: O(N*M), when theres M adventure numbers, the outer for loop runs M times and each time it runs N times for N amount of sites.
         """
         result_list = []
         for adventurer_num in adventure_numbers:
@@ -55,7 +89,13 @@ class Mode1Navigator:
 
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+            updates the site by deleting the site since there will be no land with the same ratio then set the same site with the updated stats.
+            Parameters:
+                land: the land to be updated
+                new_reward: the new reward of the land
+                new_guardians: the new guardians of the land
+            Complexity:
+                O(log(N)): the __setitem__ and __delitem__ method both have a O(logN) complexity when there's n site while the bst itself is balance.
         """
         self.sites.__delitem__(land.guardians/land.gold)
         land.gold = new_reward
--- mode2.py
+++ mode2.py
@@ -3,13 +3,24 @@ from data_structures.heap import MaxHeap
 
 class Mode2Navigator:
     """
-    Student-TODO: short paragraph as per
-    https://edstem.org/au/courses/14293/lessons/46720/slides/318306
+    This class simulates the operations of a number of adventurer teams raiding differnet sites (lands).
+    it uses MaxHeap for most of the simulation with some lists and tuples to support for storing some temporary data.
+    this class efficiently manages sites scores using max heap, ensuring the raiders always raid the land with highest score first.
+    Example: after calculation potential score of every island and returning a max heap order of 400, 300, 250. Raiders will raid the land with 400 then 300 then 250 with the get max method.
+    every time the land gets raided it will compute its potential score again and add back to the heap as it still can be better than other lands.
+    Best case complexity results for add site is O(1) when only 1 site is added and Worst case is O(N) when there's N sites added.
+    The complexity of simulate day itself is influenced by construct_score_data_structure method as it calls it initially to compute the potential scores for every site, which has a best and worst case of O(comp) and O(N*comp) respectively
+    Adding the complexity above with the complexity of the method itself, which has a best case and worst case of O(comp) and O(Klog(N)*comp) respectively,
+    we get the overall complexity of O(comp) for best and O(N*comp + Klog(N)*comp) for worst. 
     """
 
     def __init__(self, n_teams: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+            initiallizing a new object of this class with N teams
+            Parameters:
+                n_teams: the amount of adventure teams 
+            Complexity:
+                best and worst case: O(1)
         """
         self.team_count = n_teams
         self.sites = []
@@ -17,21 +28,29 @@ class Mode2Navigator:
 
     def add_sites(self, sites: list[Land]) -> None:
         """
-        Student-TODO: Best/Worst Case
+            adding all the sites and creating the max heap corresponding to the amount of sites
+            Parameters:
+                sites: a list of lands to be added
+            Complexity
+                best case: O(1), when there's only 1 site to be added
+                worst case: O(N), when there's N sites to be added
         """
-        #if len(self.sites) == 0:
         for site in sites:
             self.sites.append(site)
         self.scores = MaxHeap(len(self.sites))
-        #else:
-        #    for site in sites:
-        #        self.sites.append(site)
-        #    self.scores.heapify(len(self.sites))
 
 
     def simulate_day(self, adventurer_size: int) -> list[tuple[Land | None, int]]:
         """
-        Student-TODO: Best/Worst Case
+            simulates the day as all the teams are sent/not sent to the lands to rob gold.  Using the get max method to rob the lands with highest scores first then re-add it into our heap after.
+            Parameters:
+                adventurer_size: the adventurer size of every team for calculating the potential scores of every site and the score if not sending any.
+            Return:
+                a result which stores all the sites raided and adventurers sent on every raid.
+            Complexity:
+                Best case: if there's only 1 site and 1 team, it'll have a complexity of O(comp) where O(1) is to add the 1 site back into the heap and O(comp) to compare in compute score.
+                Worst case: O(N*comp + Klog(N)*comp) where K is the number of teams and N is number of sites, constructing the data structure will have a complexity of O(N*comp)
+                For loop runs K times as there's K teams and log(N) for add method to add the site back into the heap data structure and O(comp) to compare in compute score.
         """
         result = []
         self.construct_score_data_structure(adventurer_size)
@@ -50,6 +69,16 @@ class Mode2Navigator:
         return result
 
     def compute_score(self, site: Land, adventurer_size: int):
+        """
+            computes the potential score for the lands with the given adventurer size.
+            Parameters:
+                site: the current site to be calculated
+                adventurer_size: the adventurer size for every team that can be sent to the site
+            Returns:
+                the score of the current site and the adventurers remaining 
+            Complexity:
+                O(comp): the complexity for comparing the scores if not sending is better or looting islands is better
+        """
         default_reward = adventurer_size*2.5
         score = 0
         if site.get_guardians() > 0 and site.get_gold() > 0:
@@ -64,6 +93,19 @@ class Mode2Navigator:
             return (score, adventurers_remaining)
 
     def construct_score_data_structure(self, adventurer_size):
+        """
+            constructs the max heap data structure for all the sites according to their respective scores.
+            Parameters:
+                adventurer_size: the adventurer size for every team that can be sent to the site
+            Complexity:
+                Best case: O(comp): when there's only 1 site, it only has to compute the score and heapify that one site, for loop has O(1) complexity 
+                while comparing score has O(comp) complexity and heapify 1 site has O(1) complexity, making it's overall best case O(comp).
+                Worst case: O(N*comp): when there's N sites, for loop runs N times, compares N times which makes the complexity O(N*comp). 
+                Heapifying the score list will have a O(N) complexity aswell, making its overall worst case complexity O(N*comp) 
+        """
+        score_list = []
         for site in self.sites:
             score, adventurers_remaining = self.compute_score(site, adventurer_size)
-            self.scores.add((score, site, adventurers_remaining))
+            score_list.append((score, site, adventurers_remaining))
+
+        self.scores = MaxHeap.heapify(score_list)
